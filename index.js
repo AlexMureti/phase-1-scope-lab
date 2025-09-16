8a4d1cb8e73a2c2883e8fc6699b96a68b3161c2e
AlexMureti <alexmkmaingi@gmail.com>
2025-09-16 09:18:11 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+//declaring a variable in global scope while inside a function
+
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+}
+
+//consequently, we can overwrite the variable in the global scope
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+}
+
+//declaring a global constant then trying to change it inside a function
+const leastFavoriteCustomer = "definately not bob"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "someone else";
+}
